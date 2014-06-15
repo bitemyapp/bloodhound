e40bd933e64ff9b7bc6e2b5f8fe118dd50a81323
Michael Xavier <michael@michaelxavier.net>
2014-06-15 10:56:23 -0700

    Fix export bug

--- Database/Bloodhound/Client.hs
+++ Database/Bloodhound/Client.hs
@@ -4,7 +4,7 @@ module Database.Bloodhound.Client
        , indexExists
        , openIndex
        , closeIndex
-       , createMapping
+       , putMapping
        , deleteMapping
        , indexDocument
        , getDocument
