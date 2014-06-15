3dbcca42997227a95081651aa7d92dafedac9c2b
Chris Allen <cma@bitemyapp.com>
2014-06-14 21:27:27 -0500

    reformatting, gitignore

--- tests/tests.hs
+++ tests/tests.hs
@@ -50,8 +50,8 @@ data TweetMapping = TweetMapping deriving (Eq, Show)
 instance ToJSON TweetMapping where
   toJSON TweetMapping =
     object ["tweet" .=
-            object ["properties" .=
-                    object ["location" .= object ["type" .= ("geo_point" :: Text)]]]]
+      object ["properties" .=
+        object ["location" .= object ["type" .= ("geo_point" :: Text)]]]]
 
 exampleTweet :: Tweet
 exampleTweet = Tweet { user     = "bitemyapp"
