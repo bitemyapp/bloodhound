6e1e445c4f4de32596776c4d5d00dbe475cb828f
MHova <mho@sumall.com>
2016-02-03 17:20:28 -0800

    Add support for parsing the results of Missing Aggregations

--- src/Database/Bloodhound/Types.hs
+++ src/Database/Bloodhound/Types.hs
@@ -47,6 +47,7 @@ module Database.Bloodhound.Types
        , mkDateHistogram
        , mkDocVersion
        , docVersionNumber
+       , toMissing
        , toTerms
        , toDateHistogram
        , omitNulls
@@ -265,6 +266,7 @@ module Database.Bloodhound.Types
        , HighlightTag(..)
        , HitHighlight
 
+       , MissingResult(..)
        , TermsResult(..)
        , DateHistogramResult(..)
        , DateRangeResult(..)
@@ -1792,6 +1794,8 @@ data BucketValue = TextValue Text
                  | ScientificValue Scientific
                  | BoolValue Bool deriving (Show)
 
+data MissingResult = MissingResult { missingDocCount :: Int } deriving (Show)
+
 data TermsResult = TermsResult { termKey       :: BucketValue
                                , termsDocCount :: Int
                                , termsAggs     :: Maybe AggregationResults } deriving (Show)
@@ -1815,6 +1819,10 @@ toTerms t a = M.lookup t a >>= deserialize
 
 toDateHistogram :: Text -> AggregationResults -> Maybe (Bucket DateHistogramResult)
 toDateHistogram t a = M.lookup t a >>= deserialize
+  where deserialize = parseMaybe parseJSON
+
+toMissing :: Text -> AggregationResults -> Maybe MissingResult
+toMissing t a = M.lookup t a >>= deserialize
   where deserialize = parseMaybe parseJSON
 
 instance BucketAggregation TermsResult where
@@ -1842,6 +1850,10 @@ instance FromJSON BucketValue where
   parseJSON (Number s) = return $ ScientificValue s
   parseJSON (Bool b) = return $ BoolValue b
   parseJSON _ = mempty
+
+instance FromJSON MissingResult where
+  parseJSON = withObject "MissingResult" parse
+    where parse v = MissingResult <$> v .: "doc_count"
 
 instance FromJSON TermsResult where
   parseJSON (Object v) = TermsResult <$>
