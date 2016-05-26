b23c139077f4259249009f69717c8e5a9429eb19
winterland1989 <winterland1989@gmail.com>
2016-05-26 14:40:43 +0800

    add sniffing

--- src/Database/Bloodhound/Types/Internal.hs
+++ src/Database/Bloodhound/Types/Internal.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE DeriveGeneric              #-}
 {-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings          #-}
 
 -------------------------------------------------------------------------------
 -- |
@@ -18,13 +19,20 @@
 module Database.Bloodhound.Types.Internal
     ( BHEnv(..)
     , Server(..)
+    , newServerNoSniff
+    , newServer
+    , defSniffFreq
     , MonadBH(..)
     ) where
 
 
 import           Control.Applicative
+import           Control.Concurrent   (forkIO, threadDelay)
 import           Control.Monad.Reader
-import           Data.Text            (Text)
+import qualified Data.Aeson           as JSON
+import qualified Data.HashMap.Strict  as HM
+import qualified Data.RoundRobin      as RR
+import qualified Data.Text            as T
 import           Data.Typeable        (Typeable)
 import           GHC.Generics         (Generic)
 import           Network.HTTP.Client
@@ -43,7 +51,48 @@ instance (Functor m, Applicative m, MonadIO m) => MonadBH (ReaderT BHEnv m) wher
 
 {-| 'Server' is used with the client functions to point at the ES instance
 -}
-newtype Server = Server Text deriving (Eq, Show, Generic, Typeable)
+newtype Server = Server (RR.RoundRobin Request) deriving (Eq, Generic, Typeable)
+
+{- | make a 'Server' without cluster sniffing
+-}
+newServerNoSniff :: String -> IO Server
+newServerNoSniff addr = do
+    req <- parseUrl addr
+    rr <- RR.newRoundRobin [req]
+    return (Server rr)
+
+{- | make a 'Server' with automatic cluster sniffing, see
+    <https://www.elastic.co/guide/en/elasticsearch/reference/current/cluster-nodes-info.html>
+-}
+newServer :: String
+    -> Int      -- ^ sniffing frequence in microsecond.
+    -> Manager  -- ^ a HTTP 'Manager' is needed here, because cluster sniffing needs make HTTP request.
+    -> IO Server
+newServer addr freq mgr = do
+    req <- parseUrl addr
+    let req' = req{path = "/_nodes"}
+    rr <- sniff req' >>= RR.newRoundRobin
+    _ <-forkIO $ forever (threadDelay freq >> sniff req' >>= RR.set rr)
+    return (Server rr)
+  where
+    sniff req = do
+        res <- httpLbs req mgr
+        let hosts = do
+                JSON.Object o <- JSON.decode (responseBody res)
+                JSON.Object o' <- HM.lookup "nodes" o
+                forM (HM.elems o') $ \ (JSON.Object o'') -> do
+                    (JSON.String addr') <- HM.lookup "http_address" o''
+                    parseUrl (T.unpack $ "http://" `T.append` addr')
+        case hosts of
+            Nothing   -> error "bloodhound: sniffing failed"
+            Just []   -> error "bloodhound: sniffing no nodes"
+            Just reqs -> return reqs
+
+{- | default sniffing frequence: 15mins(15 * 60 * 10000000us).
+-}
+defSniffFreq :: Int
+defSniffFreq = 15 * 60 * 1000000
+
 
 {-| All API calls to Elasticsearch operate within
     MonadBH
