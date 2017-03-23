f4243f4750a9d8690218854053e395d858d05176
David Smith <shmish111@gmail.com>
2017-03-23 08:34:17 +0000

    fixed stats agg test

--- tests/V5/tests.hs
+++ tests/V5/tests.hs
@@ -1146,14 +1146,18 @@ main = hspec $ do
 
     it "returns stats aggregation results" $ withTestEnv $ do
       _ <- insertData
-      let stats = StatsAgg $ mkStatsAggregation $ FieldName "user"
+      let stats = StatsAgg $ mkStatsAggregation $ FieldName "age"
       let search = mkAggregateSearch Nothing $ mkAggregations "users" stats
       let search' = search { Database.V5.Bloodhound.from = From 0, size = Size 0 }
       searchExpectAggs search'
-      let docCountPair k n = (k, object ["value" .= Number n])
+      let statsAggRes k n = (k, object [ "max" .= Number n
+                                       , "avg" .= Number n
+                                       , "count" .= Number 1
+                                       , "min" .= Number n
+                                       , "sum" .= Number n])
       res <- searchTweets search'
       liftIO $
-        fmap aggregations res `shouldBe` Right (Just (M.fromList [ docCountPair "users" 1]))
+        fmap aggregations res `shouldBe` Right (Just (M.fromList [ statsAggRes "users" 10000]))
 
     it "can give collection hint parameters to term aggregations" $ when' (atleast es13) $ withTestEnv $ do
       _ <- insertData
