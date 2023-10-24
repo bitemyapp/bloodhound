0f0da12c7bdb201c34218aacdcaf0d6e1fdc3e8a
Gautier DI FOLCO <gautier.difolco@gmail.com>
2023-10-24 23:22:12 +0200

    fix(nix): use nixos-unstable (to have cache)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "bloodhound";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
